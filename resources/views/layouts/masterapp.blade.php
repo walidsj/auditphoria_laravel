2c714c1cce856d446738452fc194b97875557f71
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2020-02-02 02:57:30 +0700

    Changing locale on app
    Whats on new?
    - Locale timezone changed to Asia/Jakarta
    - Locale fallback rolled back to EN
    - Locale rolled back to EN

--- resources/views/layouts/masterapp.blade.php
+++ resources/views/layouts/masterapp.blade.php
@@ -9,6 +9,7 @@
     <meta name="robots" content="index, follow">
     <link href="" rel="canonical">
     <link rel="shortcut icon" type="image/png" href="{{ config('app.icon') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name') }}</title>
     
